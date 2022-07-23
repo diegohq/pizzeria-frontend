07b8d5d5c5b17e83810133d567d423e5c7947b57
“Diego <diegohquintale@gmail.com>
2022-07-23 18:48:03 -0300

    Button UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "../../node_modules/next/head";
 import Image from "../../node_modules/next/image";
 import logoImg from "../../public/logo.svg";
 import styles from "../../styles/home.module.scss";
+import { Button } from "../components/ui/Button/index";
 import { Input } from "../components/ui/Input/index";
 
 export default function Home() {
@@ -17,6 +18,7 @@ export default function Home() {
           <form>
             <Input type="text" placeholder="Type your e-mail" />
             <Input type="password" placeholder="Type your password" />
+            <Button type="submit" loading={false}>Login</Button>
           </form>
         </div>
 
