1bfcab30a7f74eea65735049d94f2f34116aaa88
“Diego <diegohquintale@gmail.com>
2022-10-20 18:19:01 -0300

    AuthContext

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { FormEvent, useContext } from "react";
 import logoImg from "../../public/logo.svg";
 import styles from "../../styles/home.module.scss";
 import { Button } from "../components/ui/Button/index";
 import { Input } from "../components/ui/Input/index";
+import { AuthContext } from "../contexts/AuthContext";
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+    await signIn({
+      email: "email@email.com",
+      password: "123"
+    });
+  }
+
+
   return (
     <>
       <Head>
@@ -16,7 +29,7 @@ export default function Home() {
         <Image src={logoImg} alt="Pizzeria" />
 
         <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input type="text" placeholder="Type your e-mail" />
             <Input type="password" placeholder="Type your password" />
             <Button type="submit" loading={false}>Login</Button>
